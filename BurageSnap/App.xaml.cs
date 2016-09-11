f65e99a66be722f1d787f589e93655c9c994e892
Kazuhiro Fujieda <fujieda@users.osdn.me>
2016-09-11 19:21:56 +0900

    Set the current language to English on debugging

--- BurageSnap/App.xaml.cs
+++ BurageSnap/App.xaml.cs
@@ -1,9 +1,24 @@
-﻿namespace BurageSnap
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace BurageSnap
 {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
     public partial class App
     {
+#if DEBUG
+        public App()
+        {
+            const string lang = "en";
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(lang);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(
+                XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
+#endif
     }
 }
